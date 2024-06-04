20f682ce6490a04a221b1dd65f28a37a6cd59246
Emillyvicsilva <140508800+Emillyvicsilva@users.noreply.github.com>
2024-06-04 11:07:43 -0300

    Add files via upload

---
+++ dividindo-sala.js
@@ -0,0 +1,23 @@
+const alunos = [
+  "João",
+  "Juliana",
+  "Ana",
+  "Caio",
+  "Lara",
+  "Marjorie",
+  "Guilherme",
+  "Aline",
+  "Fabiana",
+  "Andre",
+  "Carlos",
+  "Paulo",
+  "Bia",
+  "Vivian",
+  "Isabela",
+  "Vinícius",
+  "Renan",
+  "Renata",
+  "Daisy",
+  "Camilo"
+];
+
